a68745d7463a25d89f2581eea346c6ea3c8bff60
Gerecs Diana <gerecsdiana@ktch.hu>
2023-03-02 20:14:48 +0100

    database.json létrehozása

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ const nameInput = document.querySelector("#name");  //be kell kötni ezeket is, 
 const quantityInput = document.querySelector("#quantity");  //be kell kötni ezeket is, hogy a gomb reagáljon
 const priceInput = document.querySelector("#price");  //be kell kötni ezeket is, hogy a gomb reagáljon
 
+const editidInput = document.querySelector("#editid");
+const editnameInput = document.querySelector("#editname");
+const editquantityInput = document.querySelector("#editquantity");
+const editpriceInput = document.querySelector("#editprice");
+
+const saveEditButton = document.querySelector('#saveEditButton');
+
 
 const gyumolcsok = [
     { id: 1, name: 'szilva', quantity: 35, price: 8 },
@@ -29,6 +36,7 @@ function generateTbody() {
         tr.append(tdQuantity);
         tr.append(tdPrice);        
         tr.append(generateTdDelete(gyumolcs.id));
+        tr.append(generateTdEdit(gyumolcs));
     });
 }
 generateTbody();
@@ -58,6 +66,34 @@ function generateTdDelete(id) {
 }
 
 
+
+
+function generateTdEdit(fruit) {
+    let td = document.createElement('td');
+    let button = document.createElement('button');
+    button.textContent = "Szerkesztés";
+    button.classList = "btn btn-secondary";
+
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', '#editModal');
+
+
+    button.addEventListener('click', () => {
+       console.log('működik');
+       console.log(fruit.name);
+       editidInput.value = fruit.id;
+       editnameInput.value = fruit.name;
+       editquantityInput.value = fruit.quantity;
+       editpriceInput.value = fruit.price;
+
+
+    });
+    td.append(button);
+    return td;
+}
+
+
+
 saveButton.addEventListener('click', () => {
     console.log('működik');
     let name = nameInput.value;            // itt legyártjuk az objectumot
@@ -68,8 +104,40 @@ saveButton.addEventListener('click', () => {
         quantity: quantity,
         price: price
     };
-    gyumolcsok.push(gyumolcs);  //a listához hozzáadoma gyümölcsöt
+    gyumolcsok.push(gyumolcs);  //a listához hozzáadom a gyümölcsöt
     console.log(gyumolcsok);
-    tbody.textContent ='',
-    generateTbody()
-})
+    tbody.textContent ='',     
+    generateTbody();
+    clearFieldOnAddModel();   //itt hívjuk meg 
+});
+
+function clearFieldOnAddModel() {    //itt mondjuk meg, ezzel a függvénnyel, hogy törlődjön az adat mentés után az ablakból
+    nameInput.value = '';            // itt legyártjuk az objectumot
+    quantityInput.value = '';
+    priceInput.value = '';
+
+}
+
+
+saveEditButton.addEventListener('click', () => {
+    
+    let id = editidInput.value;       //kiszedjük a változásokat (meg tudjuk változtstni az adatokat)
+    let name = editnameInput.value;
+    let quantity = editquantityInput.value;
+    let price = editpriceInput.value;
+
+    gyumolcsok.forEach((gyumolcs)=>{      //megy a gyümölcs lista bejárása... s amikor egyenlő 
+        console.log(gyumolcs.name)
+        if (gyumolcs.id == id) {
+            gyumolcs.name = name;
+            gyumolcs.quantity = quantity;
+            gyumolcs.price= price;
+        }
+    });
+
+    tbody.textContent ='',      //töröljük a tbody tartalmát, és újrageneráljuk
+    generateTbody();
+
+    
+});
+
